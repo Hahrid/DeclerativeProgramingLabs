addc4efc7fc22b83753297c59ce11a0695f364de
Doctor Hack <hahridos@gmail.com>
2022-11-01 00:39:54 +0200

    aaaaa

--- Lab1/Lab1.rkt
+++ Lab1/Lab1.rkt
@@ -14,31 +14,21 @@
       (error "Error: m > n")))
 
 
-(define l '( 1 2 3 4 5 6 7 8 9 0))
+(display "Please enter M: ")
+(define m (string->number(read-line)))
+(display "Please enter N: ")
+(define n (string->number(read-line)))
 
-(define (odds lst)
-  (filter even? lst))
+(pascal-triangle m n)
 
-
-
-(define (remove-even y)
-  (cond
-    ((null? y) '())
-    ((pair? (car y))
-     (cons (remove-even (car y)) (remove-even (cdr y))))
-    ((or (not(= (remainder (car y) 2) 0))(zero? (car y))) 
-     (cons (car y) (remove-even (cdr y))))
-    (else (remove-even (cdr y)))))
-
-
-
-
+(display "Please enter list, for example ( 1 2 3 ) without brackets: ")
+(display "")
 (define x (map string->number (string-split (read-line))))
 
 
 (define (Recursion)
   (cond
-    ((null? x) "")
+    ((null? x) "END")
     ((not(= (remainder (car x) 2) 0))
     (printf "~a " (car x))
     (set! x (remove (car x) x))
@@ -49,3 +39,20 @@
 )
 
 (Recursion)
+
+
+;code for test
+
+  (define l '( 1 2 3 4 5 6 7 8 9 0))
+
+(define (odds lst)
+  (filter even? lst))
+
+(define (remove-even y)
+  (cond
+    ((null? y) '())
+    ((pair? (car y))
+     (cons (remove-even (car y)) (remove-even (cdr y))))
+    ((or (not(= (remainder (car y) 2) 0))(zero? (car y))) 
+     (cons (car y) (remove-even (cdr y))))
+    (else (remove-even (cdr y)))))
